61d4ed7dba425b418f9653e8705c8da927b1cb4d
kanczuzn <82846385+kanczuzn@users.noreply.github.com>
2022-12-03 01:47:09 -0800

    Update day5e.py

--- Day 5/day5e.py
+++ Day 5/day5e.py
@@ -15,24 +15,42 @@ pass_spc_ct = 0
 while pass_num >= pass_dig_ct and pass_spc >= pass_spc_ct:
     pass_dig_ct = 0
     pass_spc_ct = 0
+    new_pass_list = [''] * pass_len
     new_pass = ""
-    for ct in range(1,pass_len+1):
-        x = random.randint(1,3)
+    for ct in range(0, pass_len):
+        x = random.randint(1, 3)
         if x == 1:
-            y = random.randint(0,len(plist_lett)-1)
-            new_pass += plist_lett[y]
+            y = random.randint(0, len(plist_lett) - 1)
+            new_pass_list[ct] = plist_lett[y]
         elif x == 2:
-            y = random.randint(0, len(plist_dig)-1)
-            new_pass += plist_dig[y]
-            pass_dig_ct +=1
+            y = random.randint(0, len(plist_dig) - 1)
+            new_pass_list[ct] = plist_dig[y]
+            pass_dig_ct += 1
         else:
-            y = random.randint(0, len(plist_spc)-1)
-            new_pass += plist_spc[y]
-            pass_spc_ct +=1
-    if pass_num <= len(plist_dig)-1 and pass_spc <= len(plist_spc)-1:
-        for ct in range(1,len(new_pass)-1):
-            if new_pass.count(new_pass[ct]) > 2:
+            y = random.randint(0, len(plist_spc) - 1)
+            new_pass_list[ct] = plist_spc[y]
+            pass_spc_ct += 1
+    if pass_len < 1000:
+        for ct in range(0, pass_len):
+            max_lim = round((pass_len / 23) + 1)
+            new_pass += new_pass_list[ct]
+            if new_pass_list.count(new_pass_list[ct]) > max_lim:
                 pass_dig_ct = 0
                 pass_spc_ct = 0
+                ct = len(new_pass_list)
+            elif ct + 1 < pass_len:
+                if new_pass_list[ct] == new_pass_list[ct + 1]:
+                    if plist_lett.count(new_pass_list[ct + 1]) > 0:
+                        while new_pass_list[ct] == new_pass_list[ct + 1]:
+                            y = random.randint(0, len(plist_lett) - 1)
+                            new_pass_list[ct + 1] = plist_lett[y]
+                    elif plist_dig.count(new_pass_list[ct + 1]) > 0:
+                        while new_pass_list[ct] == new_pass_list[ct + 1]:
+                            y = random.randint(0, len(plist_dig) - 1)
+                            new_pass_list[ct + 1] = plist_dig[y]
+                    else:
+                        while new_pass_list[ct] == new_pass_list[ct + 1]:
+                            y = random.randint(0, len(plist_spc) - 1)
+                            new_pass_list[ct + 1] = plist_spc[y]
 
 print(f"Your new password is: {new_pass}")
